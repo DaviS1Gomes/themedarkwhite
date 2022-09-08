d84aa9460688ea52c39400774c1345ae83c9962d
Davi Gomes <99661607+DaviS1Gomes@users.noreply.github.com>
2022-09-08 06:37:19 -0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
 
 function clickButton(){
     const button = document.body.querySelector('.button');
+    const borderButton =  document.body.querySelector('.border-button') 
         button.addEventListener('click',() => {
             button.classList.toggle('animation');
             document.body.classList.toggle('dark')
+            borderButton.classList.toggle('white');
         });
 }
 clickButton();
